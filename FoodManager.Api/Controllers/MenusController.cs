03441e2a2814c43af1b74797773741800ab95e15
TechArt <ajumokoibrahimtemitope@gmail.com>
2022-06-12 08:43:28 +0100

    menu features

--- FoodManager.Api/Controllers/MenusController.cs
+++ FoodManager.Api/Controllers/MenusController.cs
@@ -1,5 +1,6 @@
 ﻿using FoodManager.Application.DTO.Menus;
 using FoodManager.Services.Abstracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodManager.Api.Controllers
@@ -8,7 +9,7 @@ namespace FoodManager.Api.Controllers
     /// This handles all requests for menus
     /// </summary>
     [Route("[controller]")]
-    //[Authorize]
+    [Authorize]
     public class MenusController : ControllerBase
     {
         private readonly ILogger<MenusController> _logger;
@@ -48,7 +49,7 @@ namespace FoodManager.Api.Controllers
         /// <param name="file"></param>
         /// <returns></returns>
         [HttpPost(Name = nameof(CreateMenu)), ProducesResponseType(typeof(GetMenuResponseObjectDto), StatusCodes.Status201Created), ProducesDefaultResponseType]
-        public async Task<IActionResult> CreateMenu([FromBody] CreateMenuDto model, IFormFile file)
+        public async Task<IActionResult> CreateMenu([FromForm] CreateMenuDto model, IFormFile file)
         {
             return Ok(await _menuService.CreateMenuAsync(model, file));
         }
